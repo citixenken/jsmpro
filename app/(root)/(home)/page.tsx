2381cec553cdc9e26072fb694065911a6a4eea9a
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-09-21 08:33:20 +0300

    Sanity: fetch data from server-side

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,12 @@
 import SearchForm from "@/components/SearchForm";
 import Filters from "@/components/Filters";
+import { getResources } from "@/sanity/actions";
 
-const Page = () => {
+const Page = async () => {
+  const resources = await getResources({ query: "", category: "", page: "1" });
+
+  console.log(resources);
+
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
