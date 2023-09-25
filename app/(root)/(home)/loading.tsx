ce9835495e12c72408a999b0ceb24bd7fec96c16
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-09-25 08:31:48 +0300

    enhancements + SEO metadata

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,14 +4,14 @@ const Loading = () => {
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen flex-col">
       <section className="nav-padding w-full">
-        <Skeleton className="h-[274px] rounded-lg bg-gray-500" />;
+        <Skeleton className="h-[274px] rounded-lg bg-black-200/40" />;
       </section>
       <section className="mt-6 flex w-full flex-col sm:mt-20">
-        <Skeleton className="h-10 w-56 bg-gray-500" />;
+        <Skeleton className="h-10 w-56 bg-black-200/40" />;
         <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-500" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-500" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-500" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
         </div>
       </section>
     </main>
