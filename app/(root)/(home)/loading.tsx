c6172a189496d3ddf9bb36e71db62c4371f7924e
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-09-25 08:04:28 +0300

    Loading page impl

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen flex-col">
+      <section className="nav-padding w-full">
+        <Skeleton className="h-[274px] rounded-lg bg-gray-500" />;
+      </section>
+      <section className="mt-6 flex w-full flex-col sm:mt-20">
+        <Skeleton className="h-10 w-56 bg-gray-500" />;
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-500" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-500" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-gray-500" />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Loading;
